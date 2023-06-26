fd7e252b90d66842e3717aa0f3bb1e5d8ba67ffe
KiraSeletska <seletskaya.kira@gmail.com>
2023-06-26 17:07:57 +0200

    .

---
+++ Frontend/Main/OnlineShop(fetch,useEffect)_15.06/products_app/src/Components/GetProductById/index.jsx
@@ -0,0 +1,75 @@
+import styles from "./styles.module.css";
+import { Product } from "../Propduct";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+export const GetProductById = () => {
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [index, setIndex] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.escuelajs.co/api/v1/products/${index}`
+        );
+        const data = await response.json();
+        setProduct(data);
+        setLoading(false);
+        console.log(data.images);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [index]);
+
+  const handleSubmit = (el) => {
+    el.preventDefault();
+  };
+
+  return (
+    <form className={styles.findIdForm} onSubmit={handleSubmit} action="">
+      <input
+        onChange={(event) => {
+          setIndex(event.target.value);
+        }}
+        type="number"
+        id="tentacles"
+        min="0"
+        max="199"
+        placeholder="Id..."
+        value={index}
+      ></input>
+      <button
+        onClick={(event) => {
+          console.log(event.target.value);
+        }}
+        type="onSubmit"
+      >
+        Find product
+      </button>
+      {loading ? (
+        <h3 className={styles.textLoading}>
+          {" "}
+          Loading <FontAwesomeIcon icon={faSpinner} spinPulse />{" "}
+        </h3>
+      ) : (
+        <div>
+          <Product
+            title={product.title}
+            description={product.description}
+            price={product.price}
+            image={product.images}
+          />
+        </div>
+      )}
+      {error ? <h3>Error: {error.message}</h3> : null}
+    </form>
+  );
+};
---
+++ Frontend/Main/UseContext_20.06/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+ 'import/prefer-default-export': 'off',
+  
+  },
+};
