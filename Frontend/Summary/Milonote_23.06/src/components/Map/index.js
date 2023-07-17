16bb90b0c3cefda093338c18a26264a8f2f9c74a
KiraSeletska <seletskaya.kira@gmail.com>
2023-07-17 16:47:37 +0200

    .

---
+++ Frontend/Summary/Milonote_23.06/src/components/Map/index.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+export const Map = () => {/*
+    const mapRef = useRef()
+
+    useEffect (() => {
+        if(!ref.current){
+            return
+        }
+    }
+    , [])*/
+}
